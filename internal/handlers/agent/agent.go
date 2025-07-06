e6d5ea70cae17de831e4b73702715b186bf479a5
Artyom Kaplin <kaplinstu1@gmail.com>
2025-07-06 18:20:17 +0300

    some commit

--- internal/handlers/agent/agent.go
+++ internal/handlers/agent/agent.go
@@ -12,175 +12,55 @@ import (
 	ms "github.com/rAch-kaplin/mipt-golang-course/MetricsService/internal/mem-storage"
 	mtr "github.com/rAch-kaplin/mipt-golang-course/MetricsService/internal/metrics"
 	log "github.com/rAch-kaplin/mipt-golang-course/MetricsService/pkg/logger"
+	rt "github.com/rAch-kaplin/mipt-golang-course/MetricsService/pkg/runtime-stats"
 )
 
-type MemRuntimeStat struct {
-	Name string
-	Type string
-	Get  func(m *runtime.MemStats) interface{}
-}
-
-var MemRuntimeStats []MemRuntimeStat = []MemRuntimeStat{
-	{
-		Name: "Alloc",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.Alloc },
-	},
-	{
-		Name: "BuckHashSys",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.BuckHashSys },
-	},
-	{
-		Name: "Frees",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.Frees },
-	},
-	{
-		Name: "GCCPUFraction",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.GCCPUFraction },
-	},
-	{
-		Name: "HeapAlloc",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.HeapAlloc },
-	},
-	{
-		Name: "HeapIdle",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.HeapIdle },
-	},
-	{
-		Name: "HeapInuse",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.HeapInuse },
-	},
-	{
-		Name: "HeapObjects",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.HeapObjects },
-	},
-	{
-		Name: "HeapReleased",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.HeapReleased },
-	},
-	{
-		Name: "HeapSys",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.HeapSys },
-	},
-	{
-		Name: "LastGC",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.LastGC },
-	},
-	{
-		Name: "Lookups",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.Lookups },
-	},
-	{
-		Name: "MCacheInuse",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.MCacheInuse },
-	},
-	{
-		Name: "MCacheSys",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.MCacheSys },
-	},
-	{
-		Name: "MSpanInuse",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.MSpanInuse },
-	},
-	{
-		Name: "MSpanSys",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.MSpanSys },
-	},
-	{
-		Name: "Mallocs",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.Mallocs },
-	},
-	{
-		Name: "NextGC",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.NextGC },
-	},
-	{
-		Name: "NumForcedGC",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.NumForcedGC },
-	},
-	{
-		Name: "NumGC",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.NumGC },
-	},
-	{
-		Name: "OtherSys",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.OtherSys },
-	},
-	{
-		Name: "PauseTotalNs",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.PauseTotalNs },
-	},
-	{
-		Name: "StackInuse",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.StackInuse },
-	},
-	{
-		Name: "StackSys",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.StackSys },
-	},
-	{
-		Name: "Sys",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.Sys },
-	},
-	{
-		Name: "TotalAlloc",
-		Type: mtr.GaugeType,
-		Get:  func(m *runtime.MemStats) interface{} { return m.TotalAlloc },
-	},
+func toFloat64(val any) (float64, bool) {
+    switch v := val.(type) {
+    case float64:
+        return v, true
+    case uint64:
+        return float64(v), true
+    case uint32:
+        return float64(v), true
+    case int:
+        return float64(v), true
+    case int64:
+        return float64(v), true
+    default:
+        return 0, false
+    }
 }
 
 func UpdateAllMetrics(storage *ms.MemStorage) {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
 
-	for _, stat := range MemRuntimeStats {
-		value := stat.Get(&memStats)
-		var metric mtr.Metric
+	for _, stat := range rt.MemRuntimeStats {
+		val := stat.Get(&memStats)
 
-		switch v := value.(type) {
-		case uint64:
-			if stat.Type == mtr.GaugeType {
-				metric = mtr.NewGauge(stat.Name, float64(v))
+		switch stat.Type {
+		case mtr.GaugeType:
+			value, ok := toFloat64(val)
+			if !ok {
+				log.Error().
+					Str("metric", stat.Name).
+					Str("type", fmt.Sprintf("%T", val)).
+					Msg("Failed to convert metric value to float64")
+				continue
 			}
-		case float64:
-			if stat.Type == mtr.GaugeType {
-				metric = mtr.NewGauge(stat.Name, v)
-			}
-		case uint32:
-			if stat.Type == mtr.GaugeType {
-				metric = mtr.NewGauge(stat.Name, float64(v))
-			}
+
+			if err := storage.UpdateMetric(mtr.NewGauge(stat.Name, value)); err != nil {
+            log.Error().
+                Err(err).
+                Str("metric", stat.Name).
+                Msg("Failed to update metric")
+        	}
 		default:
-			log.Error().Msgf("Unknown type for metric %s: %T", stat.Name, value)
-			continue
-		}
-
-		if err := storage.UpdateMetric(metric); err != nil {
-			log.Error().Msgf("Failed to update metric %s: %v", stat.Name, err)
+			 log.Error().
+                Str("metric", stat.Name).
+                Str("type", fmt.Sprintf("%T", val)).
+                Msg("Unsupported metric type")
 		}
 	}
 
