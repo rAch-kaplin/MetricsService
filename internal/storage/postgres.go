816eeb3c1b0c06f06122e25e1631f40b08bcd3a4
Kaplin Artyom <kaplinstu1@gmail.com>
2025-07-16 04:17:13 +0300

    add transaction

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -93,42 +93,7 @@ func (db *Database) GetMetric(ctx context.Context, mType, mName string) (any, er
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 
-	row := db.DB.QueryRowContext(ctx,
-		`SELECT "ID", "MType", "Delta", "Value" FROM collector `+
-			`WHERE "ID" = $1 AND "MType" = $2 LIMIT 1`, mName, mType)
-
-	var (
-		id    string
-		Type  string
-		delta sql.NullInt64
-		value sql.NullFloat64
-	)
-
-	err := row.Scan(&id, &Type, &delta, &value)
-	if errors.Is(err, sql.ErrNoRows) {
-		return nil, mtr.ErrMetricsNotFound
-	} else if err != nil {
-		return nil, fmt.Errorf("row.Scan can't read: %w", err)
-	}
-
-	switch {
-	case value.Valid:
-		if Type != mtr.GaugeType {
-			return nil, mtr.ErrInvalidMetricsType
-		}
-
-		return value.Float64, nil
-
-	case delta.Valid:
-		if Type != mtr.CounterType {
-			return nil, mtr.ErrInvalidMetricsType
-		}
-
-		return delta.Int64, nil
-	default:
-		log.Error().Msg("not valid value")
-		return nil, mtr.ErrInvalidValueType
-	}
+	return db.getMetric(ctx, mType, mName)
 }
 
 func (db *Database) GetAllMetrics(ctx context.Context) []mtr.Metric {
@@ -255,7 +220,12 @@ func (db *Database) UpdateMetric(ctx context.Context, mType, mName string, mValu
 		return fmt.Errorf("unknown metric type: %s", metric.MType)
 	}
 
-	_, err = db.DB.ExecContext(ctx,
+	tx, err := db.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("failed begin transaction")
+	}
+
+	_, err = tx.ExecContext(ctx,
 		`INSERT INTO collector ("ID", "MType", "Delta", "Value")
 		 VALUES ($1, $2, $3, $4)
 		 ON CONFLICT ("ID") DO UPDATE
@@ -266,10 +236,11 @@ func (db *Database) UpdateMetric(ctx context.Context, mType, mName string, mValu
 
 	if err != nil {
 		log.Error().Err(err).Msg("failed update insert into collector")
+		tx.Rollback()
 		return fmt.Errorf("failed update insert into collector: %w", err)
 	}
 
-	return nil
+	return tx.Commit()
 }
 
 func (db *Database) Ping(ctx context.Context) error {
