4c367f55eaebf4ef43f9f444e21c0f4912af41d5
Artyom Kaplin <kaplinstu1@gmail.com>
2025-07-27 12:19:17 +0300

    add funcs options for agent

--- cmd/agent/cfg_agent.go
+++ cmd/agent/cfg_agent.go
@@ -3,17 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"net"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
 	"time"
 
-	"github.com/caarlos0/env/v6"
 	"github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
 
+	agCfg "github.com/rAch-kaplin/mipt-golang-course/MetricsService/internal/config/agent"
 	"github.com/rAch-kaplin/mipt-golang-course/MetricsService/internal/handlers/agent"
 	repo "github.com/rAch-kaplin/mipt-golang-course/MetricsService/internal/repository"
 	auc "github.com/rAch-kaplin/mipt-golang-course/MetricsService/internal/usecases/agent"
@@ -21,112 +20,79 @@ import (
 	workerpool "github.com/rAch-kaplin/mipt-golang-course/MetricsService/pkg/worker-pool"
 )
 
-const (
-	defaultEndpoint       = "localhost:8080"
-	defaultPollInterval   = 2
-	defaultReportInterval = 10
-	defaultKey            = "key"
-	defaultRateLimit      = 3
-)
-
-type options struct {
+var (
 	endPointAddr   string
 	pollInterval   int
 	reportInterval int
-	key            string
 	rateLimit      int
-}
-
-type envConfig struct {
-	EndPointAddr   string `env:"ADDRESS"`
-	PollInterval   int    `env:"POLL_INTERVAL"`
-	ReportInterval int    `env:"REPORT_INTERVAL"`
-	Key            string `env:"KEY"`
-	RateLimit      int    `env:"RATE_LIMIT"`
-}
-
-var opts = &options{}
+	key            string
+	opts           *agCfg.Options
+)
 
 var rootCmd = &cobra.Command{
-	Use:   "agent",
-	Short: "MetricService",
-	Long:  "MetricService",
-	Args:  cobra.NoArgs,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		logFile, err := log.InitLogger("logFileAgent.log")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Logger init error: %v\n", err)
-			os.Exit(1)
-		}
-
-		defer func() {
-			if err := logFile.Close(); err != nil {
-				log.Error().Err(err).Msg("Failed to close log file")
-			}
-		}()
-
-		var cfg envConfig
-		err = env.Parse(&cfg)
-		if err != nil {
-			return fmt.Errorf("poll and report intervals must be > 0")
-		}
-
-		if cfg.EndPointAddr != "" {
-			opts.endPointAddr = cfg.EndPointAddr
-		}
-		if cfg.PollInterval > 0 {
-			opts.pollInterval = cfg.PollInterval
-		}
-		if cfg.ReportInterval > 0 {
-			opts.reportInterval = cfg.ReportInterval
-		}
-
-		if cfg.Key != "" {
-			opts.key = cfg.Key
-		}
-
-		if cfg.RateLimit > 0 {
-			opts.rateLimit = cfg.RateLimit
-		} else {
-			opts.rateLimit = defaultRateLimit
-		}
-
-		if opts.pollInterval <= 0 || opts.reportInterval <= 0 {
-			return fmt.Errorf("poll and report intervals must be > 0")
-		}
-
-		if _, _, err := net.SplitHostPort(opts.endPointAddr); err != nil {
-			return fmt.Errorf("invalid address %s: %w", opts.endPointAddr, err)
-		}
-
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		stop := make(chan os.Signal, 1)
-		signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-
-		go func() {
-			<-stop
-			cancel()
-		}()
-
-		startAgent(ctx)
-
-		return nil
-	},
+	Use:     "agent",
+	Short:   "MetricService",
+	Long:    "MetricService",
+	Args:    cobra.NoArgs,
+	PreRunE: preRunE,
+	RunE:    runE,
 }
 
 func init() {
-	opts.endPointAddr = defaultEndpoint
-	opts.pollInterval = defaultPollInterval
-	opts.reportInterval = defaultReportInterval
-	opts.key = defaultKey
+	rootCmd.Flags().StringVarP(&endPointAddr, "a", "a", agCfg.DefaultEndpoint, "endpoint HTTP-server addr")
+	rootCmd.Flags().IntVarP(&pollInterval, "p", "p", agCfg.DefaultPollInterval, "PollInterval value")
+	rootCmd.Flags().IntVarP(&reportInterval, "r", "r", agCfg.DefaultReportInterval, "PollInterval value")
+	rootCmd.Flags().StringVarP(&key, "k", "k", agCfg.DefaultKey, "key for hash")
+	rootCmd.Flags().IntVarP(&rateLimit, "l", "l", agCfg.DefaultRateLimit, "rate limit")
+}
 
-	rootCmd.Flags().StringVarP(&opts.endPointAddr, "a", "a", opts.endPointAddr, "endpoint HTTP-server addr")
-	rootCmd.Flags().IntVarP(&opts.pollInterval, "p", "p", opts.pollInterval, "PollInterval value")
-	rootCmd.Flags().IntVarP(&opts.reportInterval, "r", "r", opts.reportInterval, "PollInterval value")
-	rootCmd.Flags().StringVarP(&opts.key, "k", "k", opts.key, "key for hash")
-	rootCmd.Flags().IntVarP(&opts.rateLimit, "l", "l", opts.rateLimit, "rate limit")
+func preRunE(cmd *cobra.Command, args []string) error {
+	var err error
+	opts, err = agCfg.ParseOptionsFromCmdAndEnvs(cmd, &agCfg.Options{
+		EndPointAddr:   endPointAddr,
+		PollInterval:   pollInterval,
+		ReportInterval: reportInterval,
+		Key:            key,
+		RateLimit:      rateLimit})
+
+	opts = agCfg.NewAgentOptions(
+		agCfg.WithAddress(opts.EndPointAddr),
+		agCfg.WithPollInterval(opts.PollInterval),
+		agCfg.WithReportInterval(opts.ReportInterval),
+		agCfg.WithRateLimit(opts.RateLimit),
+		agCfg.WithKey(opts.Key),
+	)
+
+	return err
+}
+
+func runE(cmd *cobra.Command, args []string) error {
+	logFile, err := log.InitLogger("logFileAgent.log")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Logger init error: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		if err := logFile.Close(); err != nil {
+			log.Error().Err(err).Msg("Failed to close log file")
+		}
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-stop
+		cancel()
+	}()
+
+	startAgent(ctx)
+
+	return nil
 }
 
 func startAgent(ctx context.Context) {
@@ -135,9 +101,9 @@ func startAgent(ctx context.Context) {
 
 	client := resty.New().
 		SetTimeout(5 * time.Second).
-		SetBaseURL("http://" + opts.endPointAddr)
+		SetBaseURL("http://" + opts.EndPointAddr)
 
-	wp := workerpool.New(opts.rateLimit)
+	wp := workerpool.New(opts.RateLimit)
 
 	wp.Start(ctx)
 	defer wp.Wait()
@@ -147,12 +113,12 @@ func startAgent(ctx context.Context) {
 
 	go func() {
 		defer wg.Done()
-		agent.CollectMetrics(ctx, metricStorage, opts.pollInterval)
+		agent.CollectMetrics(ctx, metricStorage, opts.PollInterval)
 	}()
 
 	go func() {
 		defer wg.Done()
-		agent.SendMetrics(ctx, agentUsecase, client, wp, opts.reportInterval, opts.key)
+		agent.SendMetrics(ctx, agentUsecase, client, wp, opts.ReportInterval, opts.Key)
 	}()
 
 	<-ctx.Done()
---
+++ internal/config/agent/agent-config.go
@@ -0,0 +1,178 @@
+package agent
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/spf13/cobra"
+)
+
+const (
+	DefaultEndpoint       = "localhost:8080"
+	DefaultPollInterval   = 2
+	DefaultReportInterval = 10
+	DefaultKey            = ""
+	DefaultRateLimit      = 3
+)
+
+type Options struct {
+	EndPointAddr   string
+	PollInterval   int
+	ReportInterval int
+	Key            string
+	RateLimit      int
+}
+
+type EnvConfig struct {
+	EndPointAddr   string `env:"ADDRESS"`
+	PollInterval   int    `env:"POLL_INTERVAL"`
+	ReportInterval int    `env:"REPORT_INTERVAL"`
+	Key            string `env:"KEY"`
+	RateLimit      int    `env:"RATE_LIMIT"`
+}
+
+type Option func(*Options)
+
+func NewAgentOptions(options ...Option) *Options {
+	opts := &Options{
+		EndPointAddr:   DefaultEndpoint,
+		PollInterval:   DefaultPollInterval,
+		ReportInterval: DefaultReportInterval,
+		Key:            DefaultKey,
+		RateLimit:      DefaultRateLimit,
+	}
+
+	for _, opt := range options {
+		opt(opts)
+	}
+
+	return opts
+}
+
+func WithAddress(addr string) Option {
+	return func(o *Options) {
+		o.EndPointAddr = addr
+	}
+}
+
+func WithKey(key string) Option {
+	return func(o *Options) {
+		o.Key = key
+	}
+}
+
+func WithPollInterval(pollInterval int) Option {
+	return func(o *Options) {
+		o.PollInterval = pollInterval
+	}
+}
+
+func WithReportInterval(reportInterval int) Option {
+	return func(o *Options) {
+		o.ReportInterval = reportInterval
+	}
+}
+
+func WithRateLimit(rateLimit int) Option {
+	return func(o *Options) {
+		o.RateLimit = rateLimit
+	}
+}
+
+func ParseOptionsFromCmdAndEnvs(cmd *cobra.Command, src *Options) (*Options, error) {
+	opts, err := ParseFlags(cmd, src)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ParseEnvs(cmd, opts); err != nil {
+		return nil, err
+	}
+
+	if _, _, err := net.SplitHostPort(opts.EndPointAddr); err != nil {
+		return nil, fmt.Errorf("invalid address %s: %w", opts.EndPointAddr, err)
+	}
+
+	return opts, nil
+}
+
+func ParseFlags(cmd *cobra.Command, src *Options) (*Options, error) {
+	opts := *src
+
+	if cmd.Flags().Changed("a") {
+		opts.EndPointAddr = src.EndPointAddr
+	}
+
+	if cmd.Flags().Changed("p") {
+		if src.PollInterval > 0 {
+			opts.PollInterval = src.PollInterval
+		} else {
+			return nil, fmt.Errorf("pollInterval need > 0")
+		}
+	}
+
+	if cmd.Flags().Changed("r") {
+		if src.ReportInterval > 0 {
+			opts.ReportInterval = src.ReportInterval
+		} else {
+			return nil, fmt.Errorf("reportInterval need > 0")
+		}
+	}
+
+	if cmd.Flags().Changed("l") {
+		if src.RateLimit > 0 {
+			opts.RateLimit = src.RateLimit
+		} else {
+			return nil, fmt.Errorf("rateLimit need > 0")
+		}
+	}
+
+	if cmd.Flags().Changed("k") {
+		if src.Key != "" {
+			opts.Key = src.Key
+		}
+	}
+
+	return &opts, nil
+}
+
+func ParseEnvs(cmd *cobra.Command, opts *Options) error {
+	var cfg EnvConfig
+	err := env.Parse(&cfg)
+	if err != nil {
+		return fmt.Errorf("poll and report intervals must be > 0: %v", err)
+	}
+
+	if cfg.EndPointAddr != "" {
+		opts.EndPointAddr = cfg.EndPointAddr
+	}
+
+	if cfg.PollInterval > 0 {
+		opts.PollInterval = cfg.PollInterval
+	}
+
+	if cfg.ReportInterval > 0 {
+		opts.ReportInterval = cfg.ReportInterval
+	}
+
+	if cfg.Key != "" {
+		opts.Key = cfg.Key
+	}
+
+	if cfg.RateLimit > 0 {
+		opts.RateLimit = cfg.RateLimit
+	} else {
+		opts.RateLimit = DefaultRateLimit
+	}
+
+	if opts.PollInterval <= 0 || opts.ReportInterval <= 0 {
+		return fmt.Errorf("poll and report intervals must be > 0")
+	}
+
+	if _, _, err := net.SplitHostPort(opts.EndPointAddr); err != nil {
+		return fmt.Errorf("invalid address %s: %w", opts.EndPointAddr, err)
+	}
+
+	return nil
+}
